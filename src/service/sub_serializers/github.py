2d6a854c9d80e94e92423c6627e03e4aa215adb9
Herick Portugues <herick.portugues@gmail.com>
2022-08-24 17:22:28 -0300

    #257 Update label parameters of the Github collector serializer

--- src/service/sub_serializers/github.py
+++ src/service/sub_serializers/github.py
@@ -41,9 +41,22 @@ class GithubCollectorParamsSerializer(serializers.Serializer):
     issues_metrics_x_days = serializers.IntegerField(required=False)
     pipeline_metrics_x_days = serializers.IntegerField(required=False)
 
-    issue_labels = serializers.ListField(
-        child=serializers.CharField(max_length=100),
+    bug_label = serializers.CharField(
+        max_length=50,
         required=False,
+        help_text=(
+            'Label used to identify a bug in the given repository.'
+            'For example: BUG'
+        )
+    )
+
+    user_story_label = serializers.CharField(
+        max_length=50,
+        required=False,
+        help_text=(
+            'Label used to identify a user story in the given repository.'
+            'For example: US'
+        )
     )
 
     build_pipeline_names = serializers.ListField(
@@ -73,15 +86,9 @@ class GithubCollectorParamsSerializer(serializers.Serializer):
             'pipelines_repository_url' in data
         )
 
-        has_issue_labels_metrics_params: bool = (
-            'issue_labels' in data and
-            has_issue_metrics_params
-        )
-
         return (
             has_issue_metrics_params or
-            has_pipeline_metrics_params or
-            has_issue_labels_metrics_params
+            has_pipeline_metrics_params
         )
 
     def validate(self, attrs):
@@ -92,16 +99,8 @@ class GithubCollectorParamsSerializer(serializers.Serializer):
         ser definidos para cada uma das métricas suportadas.
         """
         metrics_required_params = {}
-
         for metric in settings.GITHUB_METRICS:
-            metric_default_name = metric['name']
-            prefix = metric_default_name.split(' in the last')[0]
-            correct_metric_name = f'{prefix} in the last X days'
-
-            if 'label' in correct_metric_name:
-                correct_metric_name = correct_metric_name.replace('bug', 'Y')
-
-            metrics_required_params[correct_metric_name] = metric['api_params']
+            metrics_required_params[metric['name']] = metric['api_params']
 
         if not self.has_at_least_one_metrics_params(attrs):
             raise serializers.ValidationError({
